84356a7dba5c3140d73eedff0163c75307116786
Ravi Katariya <97892870+ravik04@users.noreply.github.com>
2025-05-30 13:12:35 +0530

    Update utils.py

--- agents/retriever_agent/utils.py
+++ agents/retriever_agent/utils.py
@@ -4,9 +4,12 @@ from bs4 import BeautifulSoup
 def fetch_relevant_news(ticker: str):
     url = f"https://finance.yahoo.com/quote/{ticker}/news?p={ticker}"
     headers = {
-        "User-Agent": "Mozilla/5.0",
-        "Accept-Language": "en-US,en;q=0.9",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Connection": "keep-alive",
     }
+
 
     try:
         response = requests.get(url, headers=headers, timeout=10)
