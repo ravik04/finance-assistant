f713fc68f8620c24daead663412fa13d43bab302
Ravi Katariya <97892870+ravik04@users.noreply.github.com>
2025-06-03 11:28:33 +0530

    Delete api_service.py

--- api_service.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI, Query
-from typing import List
-from api_agent import get_tech_stock_data
-
-app = FastAPI()
-
-@app.get("/tech-stocks")
-def tech_stocks(tickers: List[str] = Query(...)):
-    return get_tech_stock_data(tickers)
